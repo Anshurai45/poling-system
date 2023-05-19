5105ddd51d4b3f3290c08649840ae116d3c3d52d
Anshurai45 <113780836+Anshurai45@users.noreply.github.com>
2023-05-19 23:16:11 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,4 @@
 const mongoose = require("mongoose");
-// mongoDB atlas uri
-// const URI =
-  // "mongodb+srv://shiba1999mandal:shibaram123@cluster0.nzaswbq.mongodb.net/poling-system?retryWrites=true&w=majority";
   mongoose.connect(process.env.MONGO_URI);
 // connecting to mongoDB atlas
 const connectDB = async () => {
